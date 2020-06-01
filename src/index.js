43fea36aca79ab84c2bedc7718922f7d3a66c0a2
Justin Banusing <popi1141@uw.edu>
2020-06-01 08:37:32 +0800

    quick fixes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,33 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Data from './Data.json';
 import { BrowserRouter } from 'react-router-dom';
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
 import 'firebase/database';
+import "firebase/auth";
 
+var firebaseConfig = {
+  apiKey: "AIzaSyCGiXxXvWmAR_O3hEu3oLbY63o_luOIT0Q",
+  authDomain: "loku-f7157.firebaseapp.com",
+  databaseURL: "https://loku-f7157.firebaseio.com",
+  projectId: "loku-f7157",
+  storageBucket: "loku-f7157.appspot.com",
+  messagingSenderId: "117191687967",
+  appId: "1:117191687967:web:ae54c9f1f8e3a2b7843a2a",
+  measurementId: "G-9LQSJ8SV5F"
+};
+
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// let rootRef = firebase.database().ref('Seattle');
+// rootRef.on('value', (snapshot) => {
+//   let amitValue = snapshot.val();
+//   console.log(amitValue); //=> { age: 35, petName: "Spot" }
+//   //can do something else with amitValue (e.g., assign with this.setState())
+// });
 
 ReactDOM.render(
   <BrowserRouter>
-    <App Data = {Data}/>
+    <App Data={Data}/>
   </BrowserRouter>,
   document.getElementById('root')
 );
